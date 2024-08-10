0adfe976aeb54af5c9415d632ec192cb527ae400
ochioci <63116595+ochioci@users.noreply.github.com>
2024-08-10 00:10:49 -0400

    Begin work on userID system

--- src/assets/server/index.cjs
+++ src/assets/server/index.cjs
@@ -1,15 +1,37 @@
 const express = require("express");
 const bodyParser = require('body-parser')
-
+const session = require("express-session");
 
 const PORT = process.env.PORT || 3001;
 var jsonParser = bodyParser.json()
 const app = express();
 const sqlite3 = require("sqlite3");
 app.use(jsonParser);
+app.use(session({
+    secret: 'my-secret-key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}));
 
 const db = new sqlite3.Database('./app.db');
 db.run("CREATE TABLE IF NOT EXISTS messages([message] TEXT, [email] TEXT)")
+db.run("CREATE TABLE IF NOT EXISTS estates([location] TEXT, [availability] TEXT)")
+db.run("CREATE TABLE IF NOT EXISTS users([firstname] TEXT [lastname] TEXT [email] TEXT [hashedPassword] TEXT)")
+
+
+
+//TODO: Implement login SQL, build login page,
+app.post('/login', (req, res) => {
+    // Validate user credentials
+    if (true) {
+        req.session.userId = userId; // Set session identifier
+        res.json({ message: "success", user: req.session.userId });
+    } else {
+        res.json({ message: "login failed"});
+    }
+});
+
 
 app.post("/contactAPI", jsonParser, (req, res) => {
     console.log(req.body, Object.keys(req.body));
