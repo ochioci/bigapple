700c551143cad6e0e6822edddf744650bea67670
ochioci <63116595+ochioci@users.noreply.github.com>
2024-08-09 20:31:02 -0400

    Reading and writing to contact us form now working

--- src/assets/server/index.cjs
+++ src/assets/server/index.cjs
@@ -5,13 +5,39 @@ const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3001;
 var jsonParser = bodyParser.json()
 const app = express();
+const sqlite3 = require("sqlite3");
 app.use(jsonParser);
 
+const db = new sqlite3.Database('./app.db');
+db.run("CREATE TABLE IF NOT EXISTS messages([message] TEXT, [email] TEXT)")
 
 app.post("/contactAPI", jsonParser, (req, res) => {
     console.log(req.body, Object.keys(req.body));
-    res.json({ message: "success", body: req.body });
+    res.json({ message: "success"});
+    let m = req.body.message
+    let e = req.body.email
+    db.run(`INSERT INTO messages (message, email) VALUES (?, ?)`, [m, e])
 });
+
+app.get("/getMessages", jsonParser, (req, res) => {
+    // console.log(req.body, Object.keys(req.body));
+
+    // console.log("!!!")
+    let rows = []
+    db.all("SELECT * FROM messages",
+        (error, row) => {
+            // console.log(row);
+            // rows.push(row);
+            row.forEach((r) => {
+                // console.log(r);
+                rows.push(r)
+            })
+            res.json({"messsage": "success", "rows": rows})
+        }
+    );
+    // console.log(rows)
+
+})
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
