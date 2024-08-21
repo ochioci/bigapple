768f2d97f68721f3f3928faff8bf6ff6691a69ee
ochioci <63116595+ochioci@users.noreply.github.com>
2024-08-21 11:50:31 -0400

    for frontend

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,88 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    proxy: {
+      "/contactAPI": {
+        target: "https://bigappleserver-a2c91f738c7f.herokuapp.com/",
+        secure: true,
+      },
+      "/register": {
+        target: "https://bigappleserver-a2c91f738c7f.herokuapp.com/",
+        secure: true,
+      },
+      "/login": {
+        target: "https://bigappleserver-a2c91f738c7f.herokuapp.com/",
+        secure: true,
+      },
+      "/logout": {
+        target: "https://bigappleserver-a2c91f738c7f.herokuapp.com/",
+        secure: true,
+      },
+      "/getEstates": {
+        target: "https://bigappleserver-a2c91f738c7f.herokuapp.com/",
+        secure: true,
+      },
+      "/addEstate": {
+        target: "https://bigappleserver-a2c91f738c7f.herokuapp.com/",
+        secure: true,
+      },
+      "/checkLogin": {
+        target: "https://bigappleserver-a2c91f738c7f.herokuapp.com/",
+        secure: true,
+      },
+      "/updateEstate": {
+        target: "https://bigappleserver-a2c91f738c7f.herokuapp.com/",
+        secure: true,
+      },
+      "/deleteEstate": {
+        target: "https://bigappleserver-a2c91f738c7f.herokuapp.com/",
+        secure: true,
+      },
+      "/selectEstates": {
+        target: "https://bigappleserver-a2c91f738c7f.herokuapp.com/",
+        secure: true,
+      },
+      "/selectDropoffs": {
+        target: "https://bigappleserver-a2c91f738c7f.herokuapp.com/",
+        secure: true,
+      },
+      "/getDropoffs": {
+        target: "https://bigappleserver-a2c91f738c7f.herokuapp.com/",
+        secure: true,
+      },
+      "/addDropoff": {
+        target: "https://bigappleserver-a2c91f738c7f.herokuapp.com/",
+        secure: true,
+      },
+      "/updateDropoff": {
+        target: "https://bigappleserver-a2c91f738c7f.herokuapp.com/",
+        secure: true,
+      },
+      "/deleteDropoff": {
+        target: "https://bigappleserver-a2c91f738c7f.herokuapp.com/",
+        secure: true,
+      },
+      "/getTransfers": {
+        target: "https://bigappleserver-a2c91f738c7f.herokuapp.com/",
+        secure: true,
+      },
+      "/addTransfer": {
+        target: "https://bigappleserver-a2c91f738c7f.herokuapp.com/",
+        secure: true,
+      },
+      "/updateTransfer": {
+        target: "https://bigappleserver-a2c91f738c7f.herokuapp.com/",
+        secure: true,
+      },
+      "/deleteTransfer": {
+        target: "https://bigappleserver-a2c91f738c7f.herokuapp.com/",
+        secure: true,
+      },
+      "/confirmTransfer": {
+        target: "https://bigappleserver-a2c91f738c7f.herokuapp.com/",
+        secure: true,
+      },
+    },
   },
 })
 
