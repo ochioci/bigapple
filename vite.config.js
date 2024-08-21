f79bac57d601a99d87b6aff08835b233a0200f81
ochioci <63116595+ochioci@users.noreply.github.com>
2024-08-21 11:41:49 -0400

    for node deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,88 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    proxy: {
+      "/contactAPI": {
+        target: "http://localhost:3001",
+        secure: false,
+      },
+      "/register": {
+        target: "http://localhost:3001",
+        secure: false,
+      },
+      "/login": {
+        target: "http://localhost:3001",
+        secure: false,
+      },
+      "/logout": {
+        target: "http://localhost:3001",
+        secure: false,
+      },
+      "/getEstates": {
+        target: "http://localhost:3001",
+        secure: false,
+      },
+      "/addEstate": {
+        target: "http://localhost:3001",
+        secure: false,
+      },
+      "/checkLogin": {
+        target: "http://localhost:3001",
+        secure: false,
+      },
+      "/updateEstate": {
+        target: "http://localhost:3001",
+        secure: false,
+      },
+      "/deleteEstate": {
+        target: "http://localhost:3001",
+        secure: false,
+      },
+      "/selectEstates": {
+        target: "http://localhost:3001",
+        secure: false,
+      },
+      "/selectDropoffs": {
+        target: "http://localhost:3001",
+        secure: false,
+      },
+      "/getDropoffs": {
+        target: "http://localhost:3001",
+        secure: false,
+      },
+      "/addDropoff": {
+        target: "http://localhost:3001",
+        secure: false,
+      },
+      "/updateDropoff": {
+        target: "http://localhost:3001",
+        secure: false,
+      },
+      "/deleteDropoff": {
+        target: "http://localhost:3001",
+        secure: false,
+      },
+      "/getTransfers": {
+        target: "http://localhost:3001",
+        secure: false,
+      },
+      "/addTransfer": {
+        target: "http://localhost:3001",
+        secure: false,
+      },
+      "/updateTransfer": {
+        target: "http://localhost:3001",
+        secure: false,
+      },
+      "/deleteTransfer": {
+        target: "http://localhost:3001",
+        secure: false,
+      },
+      "/confirmTransfer": {
+        target: "http://localhost:3001",
+        secure: false,
+      },
+    },
   },
 })
 
